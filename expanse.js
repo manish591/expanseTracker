deda8357dd94c9fa208f006cb2d60b3804aeb1fd
manish591 <manishdevrani777@gmail.com>
2021-07-30 17:29:00 +0530

    fix: avoid user to enter negative value

--- expanse.js
+++ expanse.js
@@ -24,6 +24,12 @@ function clearNote () {
         error.innerText = 'Please Enter A Value!';
         return;
     }
+   
+    if (Math.sign(Number(amontToPay.value)) === -1 || Number(amontToPay.value) === 0) {
+      error.innerText = 'Please enter a value 1 or more than 1!';
+      return;
+    }
+
     amontYouGave.style.display = "block";
     nextBtn.style.display = "none";
     hide.style.display = "block";
@@ -38,19 +44,34 @@ function clearNote () {
     clearNote();
     let totalBillAmount = Number(amontToPay.value);
     let totalGiven = Number(amontYouGave.value);
-    let changeTo = Number(totalGiven) - Number(totalBillAmount);
+    let changeTo = totalGiven - totalBillAmount;
     outputContainer.classList.remove('hide');
 
     if (amontYouGave.value === '') {
       error.innerText = 'Please Enter A Value!';
+      outputContainer.classList.add('hide');
+      return;
+    }
+
+    if (Math.sign(totalGiven) === -1 || totalGiven === 0) {
+      error.innerText = 'Please enter a value 1 or more than 1!';
+      outputContainer.classList.add('hide');
       return;
     }
 
     if (totalGiven < totalBillAmount) {
        error.innerText = 'Amount Given Is very Low than Bill Amount!';
+       outputContainer.classList.add('hide');
        return;
     }
 
+    if (totalGiven === totalBillAmount) {
+      error.innerText = 'No amount is left to return.';
+      outputContainer.classList.add('hide');
+      return;
+   }
+
+    error.innerText = '';
     for (let i = 0; i < currencies.length; i++) {
       if (changeTo >= currencies[i]) {
         notes[i] = Math.floor(changeTo / currencies[i]);
@@ -59,15 +80,7 @@ function clearNote () {
       }
     }
 
-    console.log(notes)
-        // for (let i = 0; i < notes.length; i++) {
-
-        //     if (notes[i]) {
-        //       console.log(`You have to pay ${notes[i]} notes of : ${currencies[i]}`)
-        //       numNote[i].innerText = `${notes[i]}`
-        //     }
-
-        //   }
+    console.log(notes);
   }
    
   calculateBtn.addEventListener('click', change);
